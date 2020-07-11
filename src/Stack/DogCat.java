3d99df7fb047936b05b38e67bf087ae853002e11
joycejoycejoyce <joycezhou757@gmail.com>
2020-07-10 23:48:17 -0400

    cat and dog problem

---
+++ src/Stack/DogCat.java
@@ -0,0 +1,119 @@
+package Stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DogCat {
+    public class Pet {
+        private String type;
+
+        public Pet (String type){
+            this.type = type;
+        }
+
+        public String getPetType(){
+            return this.type;
+        }
+    }
+
+    public class Dog extends Pet {
+        public Dog(){
+            super("dog");
+        }
+    }
+    public class Cat extends Pet {
+        public Cat(){
+            super("cat");
+        }
+    }
+
+    public class PetEnterQueue {
+        private Pet pet;
+        private  long count;
+
+        public PetEnterQueue(Pet pet, long count){
+            this.pet = pet;
+            this.count = count;
+        }
+
+        public Pet getPet(){
+            return this.pet;
+        }
+
+        public long getCount(){
+            return this.count;
+        }
+
+        public String getEnterPetType(){
+            return this.pet.getPetType();
+        }
+    }
+
+
+    public class DogCatQueue {
+        private Queue<PetEnterQueue> dogQ;
+        private Queue<PetEnterQueue> catQ;
+        private long count;
+
+        public DogCatQueue (){
+            this.dogQ = new LinkedList<>();
+            this.catQ = new LinkedList<>();
+            this.count = 0;
+        }
+
+        public void add(Pet pet){
+            if (pet.getPetType().equals("dog")){
+                this.dogQ.add(new PetEnterQueue(pet, this.count++));
+            }else if (pet.getPetType().equals("cat")){
+                this.catQ.add(new PetEnterQueue(pet, this.count++));
+            }else{
+                throw new RuntimeException("neither cat nor dog type");
+            }
+        }
+
+        public Pet pollAll(){
+            if (!this.dogQ.isEmpty() && ! this.catQ.isEmpty()){
+                if (this.dogQ.peek().getCount() < this.catQ.peek().getCount()){
+                    return this.dogQ.poll().getPet();
+                }else{
+                    return this.catQ.poll().getPet();
+                }
+            }else if (!this.dogQ.isEmpty()){
+                return this.dogQ.poll().getPet();
+            }else if (! this.catQ.isEmpty()){
+                return this.catQ.poll().getPet();
+            }else{
+                throw new RuntimeException("two queues are empty");
+            }
+        }
+
+        public Dog pollDog(){
+            if (!this.isDogQueueEmpty()){
+                return (Dog) this.dogQ.poll().getPet();
+            }else{
+                throw new RuntimeException("Dog queue is empty!");
+            }
+        }
+
+        public Cat pollCat(){
+            if(!this.isDogQueueEmpty()){
+                return (Cat) this.catQ.poll().getPet();
+            }else{
+                throw new RuntimeException("Cat queue is empty");
+            }
+        }
+
+
+        public boolean isEmpty(){
+            return this.dogQ.isEmpty() && this.catQ.isEmpty();
+        }
+
+        public boolean isDogQueueEmpty(){
+            return this.dogQ.isEmpty();
+        }
+
+        public boolean isCatQueueEmpty(){
+            return this.catQ.isEmpty();
+        }
+    }
+}
