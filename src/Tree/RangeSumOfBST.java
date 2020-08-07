6cd2903548cccec2acc01901d8e9694491aa8df2
joycejoycejoyce <joycezhou757@gmail.com>
2020-08-07 01:39:03 -0400

    range sum solved

--- src/Tree/RangeSumOfBST.java
+++ src/Tree/RangeSumOfBST.java
@@ -2,6 +2,8 @@ package Tree;
 
 import com.sun.source.tree.Tree;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 public class RangeSumOfBST {
@@ -20,46 +22,60 @@ public class RangeSumOfBST {
     }
 
 
-    int ans;
-    public int recursiveApproach(TreeNode root, int L, int R){
-        ans = 0;
-        dfs(root, L, R);
-        return ans;
+    // recursion
+    public int recursiveApproach(TreeNode root, int L, int R) {
+        return bfs(root, L, R);
     }
 
-    public void dfs(TreeNode node, int L, int R){
+    public int bfs(TreeNode node, int L, int R){
+        int sum = 0;
         if (node != null){
-            if (L <= node.val && node.val <=R){
-                ans+=node.val;
+            if (L <=node.val && node.val <=R ){
+                sum += node.val;
             }
-            if (L < node.val){
-                dfs(node.left, L, R);
+            if (L<node.val){
+                sum += bfs(node.left, L, R);
             }
             if (node.val < R){
-                dfs(node.right, L, R);
+                sum += bfs(node.right, L, R);
             }
         }
+        return sum;
     }
 
-    public int iterativeApproach(TreeNode root, int L, int R){
-        int ans = 0;
-        Stack<TreeNode> stack = new Stack<>();
+    /* Complexity:
+        T: span of L and R.
+
+        Time: O(N). In the worst case, L is the value of leftmost node. R is the value of the rightmost node
+        Space: O(H). stack takes H spaces.
+     */
+
+    public int iterativeApproach(TreeNode root, int L, int R) {
+        // create a stack
+        Deque<TreeNode> stack = new ArrayDeque<>(); // Null value cannot be inserted to ArrayDeque;
         stack.push(root);
 
-        while (!stack.isEmpty()){
-            TreeNode node = stack.pop();
-            if (node != null){
-                if (L <= node.val && node.val < R){
-                    ans += node.val;
-                }
-                if (L < node.val){
-                    stack.push(node.left);
-                }
-                if (node.val < R){
-                    stack.push(node.right);
-                }
+        int sum = 0;
+        while(!stack.isEmpty()){
+            TreeNode currentNode = stack.pop();
+
+            if (L <= currentNode.val && currentNode.val <=R){
+                sum += currentNode.val;
+            }
+            if (L < currentNode.val && currentNode.left != null){
+                stack.push(currentNode.left);
+            }
+            if (currentNode.val < R && currentNode.right != null){
+                stack.push(currentNode.right);
             }
         }
-        return ans;
+
+        return sum;
     }
+
+    /* Complexity:
+        Time: O(N)
+        Space: O(H) stack data structure. For the worst case, R is the right most node.
+                    The left side subtree of every layer is stored in the stack.
+     */
 }
